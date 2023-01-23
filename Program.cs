f9d675dbcebf31345c25a1c1e8ea55b52f3d9404
vagap <you@example.com>
2023-01-23 17:38:29 +0300

    home4

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*
+ Console.WriteLine(" text a:  ");
+ int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" text b:  ");
+ int b = Convert.ToInt32(Console.ReadLine());
+
+int size = b;
+int ddd (int num)
+{
+    for(int i = 1; i < size; i++)
+    {
+        int x = a;
+        num = x * num;
+    }
+    return num;
+}
+
+int rez = ddd(a);
+Console.WriteLine(ddd(a));
+*/
+
+
+
+/*
+int f (int N)
+{
+    int sum = 0;
+    while(N >= 1)
+    {
+       sum = sum + (N % 10);
+       N = (N - N % 10) / 10; 
+    }
+    return sum;
+}
+
+Console.WriteLine(" text a:  ");
+ int a = Convert.ToInt32(Console.ReadLine());
+
+int rez = f(a);
+Console.WriteLine(f(a));
+*/
+/*
+
+Console.WriteLine("Введите размер массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[m];
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine("Введите элемент {0}: ", i);
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("[");
+for (int i = 0; i < m; i++)
+{
+    Console.Write(arr[i] + ", ");
+}
+Console.Write("]");
+*/
+
